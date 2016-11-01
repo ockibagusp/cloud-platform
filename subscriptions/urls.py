05f2e361f361042d7772a76070a9468ec3a9841a
OckiFals <ocki.bagus.p@gmail.com>
2016-11-01 11:55:45 +0700

    update 'subscription-filter-user' url

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -5,7 +5,7 @@ from subscriptions import views
 urlpatterns = [
     url(r'^$', views.SubscriptionsList.as_view(), name="subscriptions-all"),
     url(r'^(?P<pk>[0-9]+)/$', views.SubscriptionDetail.as_view(), name="subscription-detail"),
-    url(r'^(?P<user>\w+)/$', views.SubscriptionFilterUser.as_view(), name="subscription-filter-user"),
+    url(r'^user/(?P<user>\w+)/$', views.SubscriptionFilterUser.as_view(), name="subscription-filter-user"),
     url(r'^(?P<node>\w+)/(?P<sensor>\w+)$', views.SubscriptionFilter.as_view(), name="subscription-filter"),
 ]
 

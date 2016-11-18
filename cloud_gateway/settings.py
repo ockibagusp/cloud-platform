ae96cdcd6446c55168fd0d6c1cc00e6966cb7d1f
OckiFals <ocki.bagus.p@gmail.com>
2016-11-18 14:36:42 +0700

    update template backend and template loaders

--- cloud_gateway/settings.py
+++ cloud_gateway/settings.py
@@ -63,17 +63,21 @@ ROOT_URLCONF = 'cloud_gateway.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': '',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-        },
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ],
+            'debug': DEBUG
+        }
     },
 ]
 

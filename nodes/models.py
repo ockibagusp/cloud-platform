e4b259cf99c1d3c85544cdbef4f8a1a3e9e13ecc
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2017-10-24 19:10:52 +0700

    nodes.models add 'supernode' field

--- nodes/models.py
+++ nodes/models.py
@@ -2,11 +2,13 @@ from mongoengine.document import Document
 from mongoengine import StringField, ReferenceField, EmbeddedDocumentListField, IntField, CASCADE
 from sensors.models import Sensors
 from users.models import User
+from supernodes.models import Supernodes
 # delete embeded -> Nodes.objects(label="FILKOM_1").update_one(pull__sensors__label="HUMIDITY")
 
 
 class Nodes(Document):
     user = ReferenceField(User, reverse_delete_rule=CASCADE)
+    supernode = ReferenceField(Supernodes, reverse_delete_rule=CASCADE)
     label = StringField(max_length=28)
     secretkey = StringField(required=True, max_length=16)
     is_public = IntField(default=0)

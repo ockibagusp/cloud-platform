b0fa39857ed8ab301882ff72af18775e67877ccc
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2017-12-22 11:52:28 +0700

    add coordinates field on Supernode model

--- supernodes/models.py
+++ supernodes/models.py
@@ -1,9 +1,18 @@
 from __future__ import unicode_literals
 
-from mongoengine.document import Document
-from mongoengine import StringField, ReferenceField, EmbeddedDocumentListField, CASCADE
+from mongoengine.document import Document, EmbeddedDocument
+from mongoengine import StringField, FloatField, ReferenceField, EmbeddedDocumentField, \
+    EmbeddedDocumentListField, CASCADE
 from sensors.models import Sensors
 from users.models import User
+
+
+class Coordinates(EmbeddedDocument):
+    lat = FloatField(required=True, null=False)
+    long = FloatField(required=True, null=False)
+
+    def __unicode__(self):
+        return str([self.lat, self.long])
 
 
 class Supernodes(Document):
@@ -12,6 +21,7 @@ class Supernodes(Document):
     secretkey = StringField(required=True, max_length=32)
     description = StringField(max_length=140, required=False)
     sensors = EmbeddedDocumentListField(document_type=Sensors)
+    coordinates = EmbeddedDocumentField(document_type=Coordinates, required=False)
 
     def __unicode__(self):
         return self.label
